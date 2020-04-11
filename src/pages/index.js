f469d2e48f7c3b16837e7260fc5b3265d7a831f7
1papaya <1love1papaya@gmail.com>
2020-04-11 13:12:57 +0100

    got the globe working :)

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,66 +2,87 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Globe from "../components/globe";
 
-const Index = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.siteTitle;
-  const globeRing = data.globeRing.publicURL;
-  const mapDiv = (
-    <div
-      style={{
-        borderRadius: "50%",
-        width: "100%",
-        height: "100%",
-        position: "absolute",
-        top: 0,
-        left: 0,
-      }}
-    >
-    </div>
-  );
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Layout>
-      <SEO title="home" />
-      <div
-        style={{
-          position: "absolute",
-          width: "100%",
-          height: "100%",
-          top: 0,
-          left: 0,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: "0 0.5em"
-        }}
-      >
+    this.data = props.data;
+    this.mapRef = React.createRef();
+  }
+  render() {
+    const globeRing = this.data.globeRing.publicURL;
+
+    return (
+      <Layout>
+        <SEO title="home" />
         <div
-        className="square"
           style={{
-            position: "relative",
+            position: "absolute",
             width: "100%",
-            maxWidth: 512,
-            maxHeight: 512
+            height: "100%",
+            top: 0,
+            left: 0,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "0 0.5em",
           }}
         >
-          {mapDiv}
           <div
+            className="square globe"
             style={{
-              backgroundImage: `url(${globeRing})`,
-              position: "absolute",
-              top: 0,
-              left: 0,
+              position: "relative",
               width: "100%",
-              height: "100%",
-              backgroundSize: "100% 100%",
+              maxWidth: 512,
+              maxHeight: 512,
             }}
-          ></div>
+          >
+            <div
+              ref={this.mapRef}
+              style={{
+                borderRadius: "50%",
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                top: 0,
+                left: 0,
+              }}
+            ></div>
+            <div
+              style={{
+                backgroundImage: `url(${globeRing})`,
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundSize: "100% 100%",
+              }}
+            ></div>
+          </div>
         </div>
-      </div>
-    </Layout>
-  );
-};
+      </Layout>
+    );
+  }
+
+  componentDidMount() {
+    const globe = new Globe({
+      target: this.mapRef.current,
+      places: [
+        { id: "0", address: "Denver, CO, USA", x: "-104.9903", y: "39.7392" },
+        { id: "1", address: "Boston, MA, USA", x: "-71.0589", y: "42.3601" },
+        { id: "2", address: "Mbabane, Eswatini", x: "31.1367", y: "-26.3054" },
+        { id: "3", address: "Heidelberg, Germany", x: "8.6724", y: "49.3988" },
+        { id: "4", address: "Alcudia, Spain", x: "3.124", y: "39.8533" },
+        { id: "5", address: "Vilaflor, Spain", x: "-16.6372", y: "28.1578" },
+      ],
+    });
+
+    globe.animate(31000);
+  }
+}
 
 export default Index;
 
