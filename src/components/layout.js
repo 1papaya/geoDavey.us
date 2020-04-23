e84a0b6f0d02428f6207854c3f81510a539eee2b
1papaya <1love1papaya@gmail.com>
2020-04-23 22:31:05 +0100

    o0oooo layout v-centered with little content, top if lots of content

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,49 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 
-//import "../styles/layout.scss";
+export default ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      gD_lite256: file(relativePath: { eq: "img/gD_lite.png" }) {
+        childImageSharp {
+          fixed(width: 256, height: 256) {
+            src
+          }
+        }
+      }
+    }
+  `);
 
-const Layout = ({ children }) => {
-    return (
-        <div
-        style={{
-            width: "100%",
-            height: "100%"
-        }}
-        >
-            {children}
+  return (
+    <div className="w-full h-full">
+      <div className="w-full md:min-h-full flex justify-center sm:items-start md:items-center">
+        <div className="flex m-8 rounded-lg">
+          <div className="flex flex-col mr-4" style={{ maxWidth: 80 }}>
+            <img
+              alt="geoDavey logo"
+              src={data.gD_lite256.childImageSharp.fixed.src}
+            />
+            <div className="text-sm mt-2 text-right font-palanquin">
+              <div className="p-1">home &raquo;</div>
+              <div className="p-1 hover:cursor-pointer">blog &raquo;</div>
+              <div className="p-1 hover:cursor-pointer">projects &raquo;</div>
+              <div className="p-1">contact &raquo;</div>
+              <div className="p-1 hover:cursor-pointer">&lt;3 &raquo;</div>
+            </div>
+          </div>
+
+          <div
+            className="flex p-2 flex-wrap relative rounded-lg overflow-hidden"
+            style={{
+              width: 512,
+              height: 2000,
+              background: "rgba(0,0,0,0.075)",
+            }}
+          >
+            { children }
+          </div>
         </div>
-    )
-}
-
-export default Layout
+      </div>
+    </div>
+  );
+};
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,65 +1,16 @@
 import React from "react";
 
-import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-
 
 function Home(props) {
   return (
     <Layout>
       <SEO title="home" />
 
-      <div className="w-full md:h-full flex justify-center sm:items-start md:items-center">
-        <div className="flex rounded-lg">
-          <div className="flex flex-col mr-4" style={{ maxWidth: 80 }}>
-            <img alt="geoDavey logo" src={props.data.gD_lite256.childImageSharp.fixed.src} />
-            <div className="text-sm mt-2 text-right font-palanquin">
-              <div className="p-1">home &raquo;</div>
-              <div className="p-1 hover:cursor-pointer">blog &raquo;</div>
-              <div className="p-1 hover:cursor-pointer">projects &raquo;</div>
-              <div className="p-1">contact &raquo;</div>
-              <div className="p-1 hover:cursor-pointer">&lt;3 &raquo;</div>
-            </div>
-          </div>
-
-          <div
-            className="flex flex-wrap relative rounded-lg overflow-hidden"
-            style={{
-              width: 512,
-              background: "rgba(0,0,0,0.075)"
-            }}
-          >
-            <div className="flex-auto" style={{ minHeight: 300 }}></div>
-            <div className="flex-auto w-full p-2">
-              <div className="text-2xl font-badscript">blog</div>
-            </div>
-            <div className="flex-auto w-full"></div>
-          </div>
-        </div>
-      </div>
+      <div className="font-badscript">skoobuufff</div>
     </Layout>
   );
 }
 
 export default Home;
-
-export const pageQuery = graphql`
-  query {
-    gD_lite256: file(relativePath: { eq: "img/gD_lite.png" }) {
-      childImageSharp {
-        fixed(width: 256, height: 256) {
-          src
-        }
-      }
-    }
-
-    heart128: file(relativePath: { eq: "img/heart.png" }) {
-      childImageSharp {
-        fixed(width: 128, height: 128) {
-          src
-        }
-      }
-    }
-  }
-`;
