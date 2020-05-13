22e9555602e6554380cf338f2a1e77d9e7828e45
1papaya <1love1papaya@gmail.com>
2020-05-13 13:07:46 +0100

    bot test

---
+++ src/functions/bot/bot.js
@@ -0,0 +1,23 @@
+import Telegraf from "telegraf";
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.start((ctx) => {
+  ctx.reply(`Bienvenidos al geodaveyBot!`);
+  console.log(ctx);
+});
+
+exports.handler = async (event, context, callback) => {
+  try {
+    await bot.handleUpdate(JSON.parse(event.body));
+    callback(null, {
+      statusCode: 200,
+      body: "",
+    });
+  } catch (e) {
+    callback(e, {
+      statusCode: 400,
+      body: "error: for bots only",
+    });
+  }
+};
