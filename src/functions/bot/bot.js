ce94217c5de0c1c406a0119579c19b977350fe67
1papaya <1love1papaya@gmail.com>
2020-05-13 14:11:44 +0100

    wizard test

--- src/functions/bot/bot.js
+++ src/functions/bot/bot.js
@@ -1,14 +1,42 @@
 const Telegraf = require("telegraf");
+const Stage = require('telegraf/stage');
+const WizardScene = require("telegraf/scenes/wizard");
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.start((ctx) => {
   ctx.reply(`Bienvenidos al geodaveyBot!`);
-  console.dir(ctx);
 });
 
 bot.command("chat_id", (ctx) => {
     ctx.reply(`Chat ID: ${ctx.update.message.chat.id}`)
+});
+
+//
+// update Loc
+//
+
+const updateLoc = WizardScene(
+    "update_loc",
+    ctx => {
+        ctx.reply("Where are you?");
+        return ctx.wizard.next();
+    },
+    ctx => {
+        ctx.wizard.state.location = ctx.message.location;
+        ctx.reply(`Location: ${ctx.message.location}`);
+        console.dir("message", ctx.message);
+        console.log("location", ctx.message.location);
+        returnctx.scene.leave();
+    }
+);
+
+const stage = new Stage([updateLoc]);
+
+bot.use(stage.middleware());
+
+bot.command("update_loc", ctx => {
+    ctx.scene.enter("update_loc");
 });
 
 exports.handler = async (event, context, callback) => {
