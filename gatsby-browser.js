a0a88116083ae166bc7248dcab799c3ce8b17f93
1papaya <1love1papaya@gmail.com>
2020-06-06 17:14:48 +0100

    back to es6

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,5 @@
-const { wrapWithProvider } = require("./src/state/wrap-with-provider");
-import("./src/styles/site.scss");
+import "./src/styles/site.scss";
 
-module.exports = {
-    wrapRootElement: wrapWithProvider,
-    disableCorePrefetching: () => true
-}
+import wrapWithProvider from "./src/state/wrap-with-provider"
+export const wrapRootElement = wrapWithProvider
+export const disableCorePrefetching = () => true
