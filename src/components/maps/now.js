7738dc80d52e51a5d98bfbe37602710d72434a82
1papaya <1love1papaya@gmail.com>
2020-06-10 23:42:41 +0100

    update dependencies

--- src/components/maps/now.js
+++ src/components/maps/now.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { MapContent } from "../layouts/page";
 import SEO from "../seo";
@@ -8,22 +8,32 @@ import PyreneesMap from "@1papaya/gl-pyrenees";
 import { graphql } from "gatsby";
 
 export default connect()((props) => {
+  let [map, setMap] = useState(false);
+
+  useEffect(() => {
+    import("@1papaya/gl-pyrenees").then((map) => {
+      setMap(map);
+    });
+  }, []);
+
   return (
     <MapContent>
-      <PyreneesMap
-        data={{
-          tracks: {
-            type: "FeatureCollection",
-            features: props.data.allTracks.nodes,
-          },
-          updates: {
-            type: "FeatureCollection",
-            features: props.data.allUpdates.nodes,
-          },
-        }}
-        dataBaseURL="https://gl-pyrenees.geodavey.us"
-        // onLoad={(e) => props.dispatch({ type: "TRANSITION_END" })}
-      />
+      {map && (
+        <PyreneesMap
+          data={{
+            tracks: {
+              type: "FeatureCollection",
+              features: props.data.allTracks.nodes,
+            },
+            updates: {
+              type: "FeatureCollection",
+              features: props.data.allUpdates.nodes,
+            },
+          }}
+          dataBaseURL="https://gl-pyrenees.geodavey.us"
+          onLoad={(e) => props.dispatch({ type: "TRANSITION_END" })}
+        />
+      )}
     </MapContent>
   );
 });
